897fc533fc18fc67797f1a7b35ddaf266ee8492b
Josiah <josiah.t.hartley@gmail.com>
2021-08-26 15:27:05 -0500

    Set Up Room, Added new input

---
+++ Assets/Scripts/LaunchScripts/ProjectileLauncher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ProjectileLauncher : MonoBehaviour
+{
+    [SerializeField]
+    GameObject projectile;
+    [SerializeField]
+    Vector3 ForceToApply;
+
+    private LineRenderer drawLine;
+    private void Awake()
+    {
+        drawLine = GetComponent<LineRenderer>();
+    }
+    private void FixedUpdate()
+    {
+        drawLine.SetPosition(1, ForceToApply.normalized * 3);
+    }
+}
