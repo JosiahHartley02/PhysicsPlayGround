7ec0e62699592957fd71eeba9d7dc2c1cae02af0
Josiah <josiah.t.hartley@gmail.com>
2021-08-27 16:01:05 -0500

    Added User input to affect car movement

---
+++ Assets/Scripts/VehicleBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleBehavior : MonoBehaviour
+{
+    public HingeJoint frontRight;
+    public HingeJoint frontLeft;
+    public HingeJoint backRight;
+    public HingeJoint backLeft;
+    public Vector3 Rotation;
+    private void FixedUpdate()
+    {
+        //Forward and Backwards
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            JointMotor rightmotor = frontRight.motor;
+            rightmotor.targetVelocity -= 10;
+            frontRight.motor = rightmotor;
+            backRight.motor = rightmotor;
+            JointMotor leftmotor = frontLeft.motor;
+            leftmotor.targetVelocity += 10;
+            frontLeft.motor = leftmotor;
+            backLeft.motor = leftmotor;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            JointMotor rightmotor = frontRight.motor;
+            rightmotor.targetVelocity += 10;
+            frontRight.motor = rightmotor;
+            JointMotor leftmotor = frontLeft.motor;
+            leftmotor.targetVelocity -= 10;
+            frontLeft.motor = leftmotor;
+        }
+
+        //Steering
+
+    }
+}
