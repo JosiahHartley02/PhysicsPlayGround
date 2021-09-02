60d57db4a498279150ccc0adbb6d042bb2030317
Josiah <josiah.t.hartley@gmail.com>
2021-09-02 10:32:37 -0500

    Added collision detection for the camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public float distanceFromTarget = 5.0f;
+    public float maxDistance = 5.0f;
     public float sensitivity = 2.0f;
+    public float relaxSpeed = 5.0f;
     public bool invertY = false;
     public bool invertX = false;
+
+    private float currentDistance = 0.0f;
+
+    private void Start()
+    {
+        currentDistance = maxDistance;
+    }
     private void Update()
     {
         //Rotate the camera
         if(Input.GetMouseButton(1))
         {
+            //Store current angles
             Vector3 angles = transform.eulerAngles;
+            //Get input
             Vector2 rotation;
             rotation.x = Input.GetAxis("Mouse Y") * (invertY ? 1.0f : -1.0f);
             rotation.y = Input.GetAxis("Mouse X") * (invertX ? 1.0f : -1.0f);
             //Look up and down by rotating around the X-axis
-            angles.x = Mathf.Clamp(angles.x + rotation.x * sensitivity, 0.0f,70.0f);
+            angles.x = Mathf.Clamp(angles.x + rotation.x * sensitivity, 0.0f,80.0f);
             //Look left and right by rotating around the Y-axis
             angles.y += rotation.y * sensitivity;
-            //Set the angles
+            //Set the updated angles
             transform.eulerAngles = angles;
         }
         //Move the camera
-        transform.position = target.position + (distanceFromTarget * -transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(target.position, -transform.forward, out hitInfo, maxDistance))
+        {
+            currentDistance = Mathf.MoveTowards(currentDistance, hitInfo.distance, Time.deltaTime * relaxSpeed * (maxDistance / currentDistance));
+        }
+        else
+            currentDistance = Mathf.MoveTowards(currentDistance, maxDistance, Time.deltaTime * relaxSpeed * (maxDistance/ currentDistance));
+        
+        transform.position = target.position + (currentDistance * -transform.forward);
     }
 }
