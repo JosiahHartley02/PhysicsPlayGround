3b13e50fc20fe91a82bef07e478967685268916c
Josiah <josiah.t.hartley@gmail.com>
2021-09-03 15:57:44 -0500

    Vehicle Turning Close to Functional, Buggy due to over rotation lack of clamp

--- Assets/Scripts/VehicleBehavior.cs
+++ Assets/Scripts/VehicleBehavior.cs
@@ -17,6 +17,9 @@ public class VehicleBehavior : MonoBehaviour
     private JointMotor backRightMotor;
     private JointMotor backLeftMotor;
 
+
+    public float desiredTireRotation = 20;
+
     private void Update()
     {
         if (!activePlayer)
@@ -26,16 +29,38 @@ public class VehicleBehavior : MonoBehaviour
         frontLeftMotor = frontLeftWheel.motor;
         backRightMotor = backRightWheel.motor;
         backLeftMotor = backLeftWheel.motor;
-        
-        float InputRight = Input.GetAxis("Horizontal");
-        float InputForward = Input.GetAxis("Vertical");
 
+        //Getting a reference to the front two wheels
+        GameObject frontRightTire = frontRightWheel.connectedBody.gameObject;
+        GameObject frontLeftTire = frontLeftWheel.connectedBody.gameObject;
+        
+        float InputForward = Input.GetAxis("Vertical");
+        float InputRight = Input.GetAxis("Horizontal");
 
         //Uniform 4 wheel drive acceleration and decceleration
         frontRightMotor.targetVelocity -= InputForward;
         backRightMotor.targetVelocity -= InputForward;
         frontLeftMotor.targetVelocity += InputForward;
         backLeftMotor.targetVelocity += InputForward;
+
+        //If the player turns left
+        //INTRODUCE CLAMPING TO EACH FUNCTION TO PREVENT OVER ROTATING
+        if (InputRight < 0)
+        {
+            frontRightTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontRightTire.transform.rotation.z, desiredTireRotation, 0.3f)));
+            frontLeftTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontLeftTire.transform.rotation.z, desiredTireRotation, 0.3f)));
+        }
+        else if (InputRight > 0)
+        {
+            frontRightTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontRightTire.transform.rotation.z, -desiredTireRotation, 0.3f)));
+            frontLeftTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontLeftTire.transform.rotation.z, -desiredTireRotation, 0.3f)));
+        }
+        else
+        {
+            frontRightTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontRightTire.transform.rotation.z, 0, 0.15f)));
+            frontLeftTire.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(frontLeftTire.transform.rotation.z, 0, 0.15f)));
+        }
+
 
         //Applying modified values to motors on the hinges
         frontRightWheel.motor = frontRightMotor;
